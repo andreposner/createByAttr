d95f196ee34d83ed4078114e32d68a65df1b3c9b
Jakob Bachhuber <extern.jakob.bachhuber@itzbund.de>
2019-11-14 12:55:35 +0100

    Logging verbessert: Kein Logging mehr in catalina.out

--- createByAttr/src/main/java/org/forgerock/auth/createByAttr/createByAttr.java
+++ createByAttr/src/main/java/org/forgerock/auth/createByAttr/createByAttr.java
@@ -25,8 +25,10 @@
     import org.forgerock.openam.annotations.sm.Attribute;
     import org.forgerock.openam.auth.node.api.*;
     import org.forgerock.openam.core.CoreWrapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-    import javax.inject.Inject;
+import javax.inject.Inject;
     import javax.lang.model.type.NullType;
 
     import java.util.*;
@@ -51,7 +53,7 @@
         private static String attrValue = "";
         private static String username = "";
 
-        protected Debug debug = Debug.getInstance(DEBUG_FILE);
+        private static  final Logger logger = LoggerFactory.getLogger("createByAttr");
 
         /**
          * Configuration for the node.
@@ -81,25 +83,20 @@
             this.coreWrapper = coreWrapper;
         }
 
-        public void debugmessage(String s) {
-            System.out.println(s);
-        }
 
         @Override
         public Action process(TreeContext context) throws NodeProcessException {
-            debugmessage("[" + DEBUG_FILE + "]: createByAttr started ...");
-            debugmessage("[" + DEBUG_FILE + "]: Configuration: shared state attribute: '" + config.sharedStateVar() + "'.");
-            debugmessage("[" + DEBUG_FILE + "]: Configuration: default password: '" + config.defaultPwd() + "'.");
+            logger.debug("createByAttr :: initiate Node :: password attribute : {}", config.passwordAttribute());
+            logger.debug("createByAttr :: initiate Node :: shared state attribute : {}", config.sharedStateVar());
+            logger.debug("createByAttr :: initiate Node :: default password : {}", config.defaultPwd());
+
 
             attrValue = context.sharedState.get(config.sharedStateVar()).toString().replace("\"", "");
+            if (logger.isWarnEnabled()) {
+                logger.warn("createByAttr :: found pseudonym : {}", attrValue);
+            }
+            
 
-            debugmessage("[" + DEBUG_FILE + "]: Found restricted ID: '" + attrValue + "'.");
-
-            debugmessage("[" + DEBUG_FILE + "]: Will create user with BaseDN: '" + coreWrapper.convertRealmPathToRealmDn(context.sharedState.get(REALM).asString()) + "'.");
-            debugmessage("[" + DEBUG_FILE + "]: and id: '" + attrValue + "'.");
-
-            debugmessage("[" + DEBUG_FILE + "]: and '" + config.passwordAttribute() + "' set to '" + config.defaultPwd() + "'.");
-            debugmessage("[" + DEBUG_FILE + "]: and value of '" + config.sharedStateVar() + "' will be written to '" + config.storingAttribute() + "' in user's profile.");
 
             Set<String> pwdValue = new HashSet<>();
             pwdValue.add(config.defaultPwd());
@@ -113,10 +110,15 @@
 
             AMIdentityRepository idrepo = coreWrapper.getAMIdentityRepository(
                     coreWrapper.convertRealmPathToRealmDn(context.sharedState.get(REALM).asString()));
+
+            logger.debug("createByAttr :: user creation :: create user with BaseDN : {} and id: {} and {}: {}", coreWrapper.convertRealmPathToRealmDn(context.sharedState.get(REALM).asString()), attrValue, config.passwordAttribute(), config.defaultPwd());
+            logger.debug("createByAttr :: user creation :: Shared state attribute {} wil be written to {} in users's profile.", config.sharedStateVar(), config.storingAttribute());
             try {
                 AMIdentity id = idrepo.createIdentity(IdType.USER, attrValue, attrMap);
-                debugmessage("[" + DEBUG_FILE + "]: User successfully created.");
-                // context.sharedState.put(USER_GOTO_PARAM_KEY, "http://www.google.de");
+                if (logger.isWarnEnabled()) {
+                    logger.warn("createByAttr :: User successfully created");
+                }
+             
                 return goTo(true).build();
             } catch (IdRepoException e) {
             e.printStackTrace();
